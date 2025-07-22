8462e56637cbb06c05e4d739926b10a8287972a0
yamanoku <0910yama@gmail.com>
2025-07-22 14:50:18 +0900

    update highlightjs, code padding style

--- pages/_includes/layout.11ty.js
+++ pages/_includes/layout.11ty.js
@@ -14,10 +14,10 @@ exports.render = (data) => {
         <meta name="twitter:image:alt" content="${data.title}">
         <meta property="og:locale" content="${data.lang}">
         <link rel="stylesheet" href="https://cdn.skypack.dev/yama-normalize">
-        <script src="https://unpkg.com/@highlightjs/cdn-assets@11.6.0/highlight.min.js"></script>
-        <link rel="stylesheet" href="https://unpkg.com/@highlightjs/cdn-assets@11.6.0/styles/a11y-dark.min.css">
-        <script type="module">hljs.initHighlightingOnLoad()</script>
-        <style>code{font-family:SFMono-Regular,Consolas,Liberation Mono,Menlo,monospace;font-size:1em;}pre code.hljs{padding:calc(var(--rhythm)*3);}footer{padding: var(--y-rhythm-3);text-align: center;}</style>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.11.1/highlight.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.11.1/styles/a11y-dark.min.css">
+        <script type="module">hljs.highlightAll();</script>
+        <style>img{height:auto}code{font-family:SFMono-Regular,Consolas,Liberation Mono,Menlo,monospace;font-size:1em;}pre code.hljs{padding:var(--y-rhythm-3)}footer{padding: var(--y-rhythm-3);text-align: center;}</style>
       </head>
       <body>
         <main id="main">
